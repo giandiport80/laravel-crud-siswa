be84b9ce9cf8545d1d63871e9ef10c4c99175304
giannurwana19 <giannurwana19@gmail.com>
2020-08-11 11:29:24 +0700

    login sistem manual #13

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.main')
+
+@section('content')
+    <h1>ini dashboard</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/siswa', 'SiswaController@index')->name('siswa.index');
-Route::post('/siswa', 'SiswaController@store')->name('siswa.store');
-Route::get('/siswa/{siswa}/edit', 'SiswaController@edit')->name('siswa.edit');
-Route::patch('/siswa/{siswa}', 'SiswaController@update')->name('siswa.update');
-Route::delete('/siswa/{siswa}', 'SiswaController@destroy')->name('siswa.destroy');
+Route::get('/login', 'AuthController@login')->name('login');
+Route::post('/login', 'AuthController@authLogin')->name('auth.post');
+Route::get('/logout', 'AuthController@authLogout')->name('auth.logout');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
+    Route::get('/siswa', 'SiswaController@index')->name('siswa.index');
+    Route::post('/siswa', 'SiswaController@store')->name('siswa.store');
+    Route::get('/siswa/{siswa}/edit', 'SiswaController@edit')->name('siswa.edit');
+    Route::patch('/siswa/{siswa}', 'SiswaController@update')->name('siswa.update');
+    Route::delete('/siswa/{siswa}', 'SiswaController@destroy')->name('siswa.destroy');
+});
