20dfefaef979616c8221c8d013ece0c7090c5eeb
giannurwana19 <giannurwana19@gmail.com>
2020-08-14 22:08:45 +0700

    halaman muka website #25

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('home');
-// });
+Route::get('/', 'SiteController@index')->name('site.index');
+Route::get('/about', 'SiteController@about')->name('site.about');
 
-Route::get('/', 'AuthController@login')->name('login');
+// Route::get('/', 'AuthController@login')->name('login');
 Route::get('/login', 'AuthController@login')->name('login');
 Route::post('/login', 'AuthController@authLogin')->name('auth.post');
 Route::get('/logout', 'AuthController@authLogout')->name('auth.logout');
